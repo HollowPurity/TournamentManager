38cf17bcfab6d95e9dbf1bc240525219ba9699ed
1925458 <1925458tud.hs-mannheim.de>
2024-05-20 20:23:55 +0200

    test: Can new Project Be Created

---
+++ src/turnier/MenuTest.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package turnier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import turnier.menu.CLIMenu;
+
+/**
+ * 
+ */
+class MenuTest {
+
+	private Menu myMenu;
+	final String VALID_FILE_NAME = "MyProject";
+	final String EMPTY_FILE_NAME = "";
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		myMenu = new CLIMenu();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+		myMenu.removeProject(VALID_FILE_NAME);
+	}
+	
+	@Test
+	void testCreateNewProject() {
+		myMenu.createNewProject(VALID_FILE_NAME);
+		File f = new File(VALID_FILE_NAME + ".prj");
+		assertTrue(f.exists() && !f.isDirectory());
+	}
+
+
+}
